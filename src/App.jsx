8d255873e6f5841e1e44fa4771b5fd5ee7bddf88
BalrajMahto <161921206+BalrajMahto@users.noreply.github.com>
2025-07-12 23:57:31 +0530

    working on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import  Layout  from './pages/Layout.jsx'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitle from './pages/BlogTitle'
+import GenerateImages from './pages/GenerateImages.jsx'
+import RemoveBackground from './pages/RemoveBackground.jsx'
+import RemoveObject from './pages/RemoveObject.jsx'
+import ReviewResume from './pages/ReviewResume.jsx'
+import Community from './pages/Community.jsx'
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/ai' element={<Layout />} >
+        <Route index element={<Dashboard />} />
+        <Route path='write-article' element={<WriteArticle/>} />
+        <Route path='blog-titles' element={<BlogTitle />} />
+        <Route path='generate-images' element={<GenerateImages />} />
+        <Route path='remove-background' element={<RemoveBackground />} />
+        <Route path='remove-object' element={<RemoveObject />} />
+        <Route path='review-resume' element={<ReviewResume />} />
+        <Route path='community' element={<Community />} />
+
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
