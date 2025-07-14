a0cc98c8a6267822135390b9c1ca246db29eae23
BalrajMahto <161921206+BalrajMahto@users.noreply.github.com>
2025-07-14 23:31:36 +0530

    sidebar creation of different pages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
   return (
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
         <div className='text-center mb-6'>
-            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content <br /> with <span className='relative bg-gradient-to-r from-blue-800 to-violet-400 text-transparent bg-clip-text text-nowrap'>AI tools</span></h1>
+            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create Amazing Content <br /> with <span className='relative bg-gradient-to-r from-blue-800 to-violet-400 text-transparent bg-clip-text text-nowrap'>AI tools</span></h1>
             <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'>Transform yout content with our suite of premium AI tools. Write articles, generate images, and enhance your work flow.</p>
         </div>
 
