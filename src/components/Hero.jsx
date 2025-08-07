9d0673c23bdd566674d7f14e3c90c61ebb70c195
BalrajMahto <161921206+BalrajMahto@users.noreply.github.com>
2025-08-07 22:35:04 +0530

    frontend completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 
-
 const Hero = () => {
 
     const navigate = useNavigate()
 
   return (
+    
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-cover bg-no-repeat min-h-screen'>
         <div className='text-center mb-6'>
             <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create Amazing Content <br /> with <span className='relative bg-gradient-to-r from-blue-800 to-violet-400 text-transparent bg-clip-text text-nowrap'>AI tools</span></h1>
